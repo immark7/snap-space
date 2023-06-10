1d60008c03ecab4b9194349a4697102ce927391f
Thanh Huu Phan (3073) <thanh.phan@visily.ai>
2023-06-10 09:33:17 +0700

    feat: init pages

---
+++ app/generator/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Work in progress</div>
+  )
+}
+
+export default page
--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,18 +1,56 @@
+"use client";
+
 import { AirVent, GithubIcon } from "lucide-react";
 import React from "react";
 import Link from "next/link";
 import ModeToggle from "./ModeToggle";
 import { buttonVariants } from "../ui/button";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-30 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center justify-between">
-        <div className="flex items-center space-x-3">
+        <Link href="/" className="mr-8 flex items-center space-x-2">
           <AirVent className="w-6 h-6" />
-          <h2 className="font-bold">Snapspace</h2>
-        </div>
+          <span className="hidden font-bold sm:inline-block">Snapspace</span>
+        </Link>
+        <nav className="flex items-center gap-6 text-sm font-medium">
+          <Link
+            href="/"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname === "/" ? "text-foreground" : "text-foreground/60"
+            )}
+          >
+            Search Image
+          </Link>
+          <Link
+            href="/generator"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname?.startsWith("/generator")
+                ? "text-foreground"
+                : "text-foreground/60"
+            )}
+          >
+            AI Generator
+          </Link>
+          <Link
+            href="/variant"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname?.startsWith("/variant")
+                ? "text-foreground"
+                : "text-foreground/60"
+            )}
+          >
+            AI Variant
+          </Link>
+        </nav>
 
         <div>
           <Link
